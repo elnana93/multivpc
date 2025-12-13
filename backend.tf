888049e30a64aead5027d9832a9fe3ddd1038051
elnana93 <e5leadsusa@gmail.com>
2025-12-12 16:18:40 -0800

    backend.tf

---
+++ backend.tf
@@ -0,0 +1,7 @@
+ terraform {
+  backend "s3" {
+    bucket = "e5statefiles"
+    key    = "multivpc/terraform.tfstate"
+    region = "us-west-2"  # <--- MAKE SURE THIS MATCHES YOUR BUCKET REGION
+  }
+} 
